a79a9f5f4f13fd2c0ecf1462541102c4f4a2119f
Weverson Nascimento <weverson.sn@gmail.com>
2017-03-06 17:08:37 -0300

    Create tree.pl

---
+++ tree.pl
@@ -0,0 +1,56 @@
+% Fatos
+progenitor(maria, josé).
+progenitor(joão, josé).
+progenitor(joão, ana).
+progenitor(josé, júlia).
+progenitor(josé, íris).
+progenitor(íris, jorge).
+progenitor(ana, fernando).
+
+masculino(joão).
+masculino(josé).
+masculino(jorge).
+
+
+feminino(maria).
+feminino(júlia).
+feminino(ana).
+feminino(íris).
+
+% Regras
+
+filho(Y, X) :- 
+    progenitor(X, Y).
+
+mãe(X, Y) :-
+    progenitor(X, Y),
+    feminino(X).
+
+avô(X, Z) :
+    progenitor(X, Y),
+    progenitor(Y, Z).
+
+irmã(X, Y) :-
+    progenitor(Z, X),
+    progenitor(Z, Y),
+    feminino(X),
+    dif(X, Y).
+
+tio(X, Y) :
+    progenitor(Z, Y),
+    progenitor(H, Z),
+    progenitor(H, X).
+
+prima(X, Y) :-
+    progenitor(Z1, X),
+    progenitor(Z2, Y),
+    progenitor(Z, z1),
+    progenitor(Z, z2),
+    feminino(X).
+    
+antepassado(X, Z) :-
+    progenitor(X, Z).
+
+antepassado(X, Z) :- 
+    progenitor(X, Y),
+    antepassado(Y, Z).
