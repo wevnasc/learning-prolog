927e850107f2013d78f2034f174db74b8d0631ac
Weverson Nascimento <weverson.sn@gmail.com>
2017-03-20 21:04:10 -0300

    Update controle_procedimental.pl

--- controle_procedimental.pl
+++ controle_procedimental.pl
@@ -1,3 +1,22 @@
-f(X, 0) :- X < 5.
-f(X, 1) :- X >= 5, X =< 9.
+f(X, 0) :- X < 5, !.
+f(X, 1) :- X >= 5, X =< 9, !.
 f(X, 2) :- X > 9.
+
+par_ou_impar(X) :- X mod 2 =:= 0 -> write(par); write(impar).
+
+d(0).
+d(1).
+bin :- d(A), d(B), d(C), write([A,B,C]), nl, fail.
+
+joga(ana,volei).
+joga(bia,tenis).
+joga(ivo,basquete).
+joga(eva,volei).
+joga(leo,tenis).
+
+%joga(P, X), joga(leo, X), P \= leo
+%joga(leo, X), joga(P, X), P \= leo.
+
+num(N,positivo) :- N>0, !.
+num(N,negativo) :- N<0, !.
+num(0,nulo).
